ae21877e58c03d5ee835804653cb02025d5f3ecf
Sanka Samaranayake <sanka.samaranayake@activeeon.com>
2013-05-31 11:09:52 +0200

    Changed the logic to retrieve stats from summary.quickStats MOB instead of using performance counters.

--- src/org/ow2/proactive/iaas/vcloud/monitoring/VimServiceConstants.java
+++ src/org/ow2/proactive/iaas/vcloud/monitoring/VimServiceConstants.java
@@ -40,74 +40,74 @@ package org.ow2.proactive.iaas.vcloud.monitoring;
 public class VimServiceConstants {
 
     // Static properties of ClusterComputeResource objects.
-	public static final String PROP_CCR_HOST = "host";
-	
-	// Static properties which are common to both host systems and virtual
-	// machines.
-	public static final String PROP_NAME = "name";
-	public static final String PROP_STATE = "summary.runtime.powerState";
+    public static final String PROP_CCR_HOST = "host";
 
-	
-	// Static properties of host systems.
-	public static final String PROP_HOST_CPU_FREQUENCY = "summary.hardware.cpuMhz";
-	public static final String PROP_HOST_CPU_CORES = "summary.hardware.numCpuCores";
-	public static final String PROP_HOST_MEMORY_TOTAL = "summary.hardware.memorySize";
-	public static final String PROP_HOST_MEM_USED = "summary.quickStats.overallMemoryUsage"; 
-	public static final String PROP_HOST_NETWORK_COUNT = "summary.hardware.numNics";
-	public static final String PROP_HOST_SYSTEM_IDENTIFICATION = "hardware.systemInfo.otherIdentifyingInfo";
-	public static final String PROP_HOST_SITE = "parent";
-	public static final String PROP_HOST_DS = "datastore";
-	
-	
-	// Static properties of datastores
-	public static final String PROP_DS_TYPE = "summary.type";
-	public static final String PROP_DS_CAPACITY = "summary.capacity";
-	public static final String PROP_DS_FREE_SPACE = "summary.freeSpace";
+    // Static properties which are common to both host systems and virtual
+    // machines.
+    public static final String PROP_NAME = "name";
+    public static final String PROP_STATE = "summary.runtime.powerState";
 
-	// Static properties for VMs.
-	public static final String PROP_VM_CPU_CORES = "summary.config.numCpu";
-	public static final String PROP_VM_CPU_FREQUENCY = "summary.quickStats.staticCpuEntitlement";
-	public static final String PROP_VM_MEMEORY_TOTAL = "summary.config.memorySizeMB";
-	public static final String PROP_VM_STORAGE_COMMITTED = "summary.storage.committed";
-	public static final String PROP_VM_STORAGE_UNCOMMITTED = "summary.storage.uncommitted";
-	public static final String PROP_VM_NETWORK = "summary.config.numEthernetCards";
-	public static final String PROP_VM_PARENT = "summary.runtime.host";
-	public static final String PROP_VM_DISK = "guest.disk";
+    // Static properties of host systems.
+  
+    /*
+     * The speed of the CPU cores. This is an average value if there are
+     * multiple speeds. The product of cpuMhz and numCpuCores is approximately
+     * equal to the sum of the MHz for all the individual cores on the host.
+     */
+    public static final String PROP_HOST_CPU_FREQUENCY = "summary.hardware.cpuMhz";
+    
+    public static final String PROP_HOST_CPU_CORES = "summary.hardware.numCpuCores";
+    public static final String PROP_HOST_CPU_USAGE = "summary.quickStats.overallCpuUsage";
+    public static final String PROP_HOST_MEMORY_TOTAL = "summary.hardware.memorySize";
+    public static final String PROP_HOST_MEMORY_USAGE = "summary.quickStats.overallMemoryUsage";
+    public static final String PROP_HOST_NETWORK_COUNT = "summary.hardware.numNics";
+    public static final String PROP_HOST_SYSTEM_IDENTIFICATION = "hardware.systemInfo.otherIdentifyingInfo";
+    public static final String PROP_HOST_SITE = "parent";
+    public static final String PROP_HOST_DS = "datastore";
 
-	/** Dynamic properties for both host systems and virtual machines. */
+    // Static properties of datastores
+    public static final String PROP_DS_TYPE = "summary.type";
+    public static final String PROP_DS_CAPACITY = "summary.capacity";
+    public static final String PROP_DS_FREE_SPACE = "summary.freeSpace";
 
-	// CPU usage as a percentage during the interval
-	public static final String PROP_CPU_USAGE = "cpu.usage.AVERAGE";
-	// Amount of memory that is actively used, as estimated by VMkernel based on
-	// recently touched memory pages
-	public static final String PROP_MEM_USAGE = "mem.active.AVERAGE";
-	// Average amount of data received per second
-	public static final String PROP_NET_RX_RATE = "net.bytesRx.AVERAGE";
-	// Average amount of data transmitted per second
-	public static final String PROP_NET_TX_RATE = "net.bytesTx.AVERAGE";
-	// Network utilization (combined transmit-rates and receive-rates) during
-	// the interval
-	public static final String PROP_NET_USAGE = "net.usage.AVERAGE";
+    // Static properties for VMs.
+    public static final String PROP_VM_CPU_CORES = "summary.config.numCpu";
+    public static final String PROP_VM_CPU_FREQUENCY = "summary.runtime.maxCpuUsage";
+    public static final String PROP_VM_CPU_USAGE = "summary.quickStats.overallCpuUsage";
+    public static final String PROP_VM_MEMEORY_TOTAL = "summary.config.memorySizeMB";
+    public static final String PROP_VM_MEMORY_USAGE = "summary.quickStats.guestMemoryUsage";
+//    public static final String PROP_VM_STORAGE_COMMITTED = "summary.storage.committed";
+//    public static final String PROP_VM_STORAGE_UNCOMMITTED = "summary.storage.uncommitted";
+    public static final String PROP_VM_NETWORK = "summary.config.numEthernetCards";
+    public static final String PROP_VM_PARENT = "summary.runtime.host";
+    public static final String PROP_VM_DISK = "guest.disk";
+
+    /** Dynamic properties for both host systems and virtual machines. */
+    // Average amount of data received per second
+    public static final String PROP_NET_RX_RATE = "net.bytesRx.AVERAGE";
+    // Average amount of data transmitted per second
+    public static final String PROP_NET_TX_RATE = "net.bytesTx.AVERAGE";
+    // Network utilization (combined transmit-rates and receive-rates) during
+    // the interval
+    public static final String PROP_NET_USAGE = "net.usage.AVERAGE";
 
     public static final String[] HOST_STATIC_PROPERTIES = new String[] {
-            PROP_HOST_CPU_CORES, PROP_HOST_CPU_FREQUENCY,
-            PROP_HOST_MEMORY_TOTAL, PROP_HOST_MEM_USED,
+            PROP_HOST_CPU_CORES, PROP_HOST_CPU_FREQUENCY, PROP_HOST_CPU_USAGE,
+            PROP_HOST_MEMORY_TOTAL, PROP_HOST_MEMORY_USAGE,
             PROP_HOST_NETWORK_COUNT, PROP_HOST_SITE, PROP_STATE, PROP_HOST_DS };
 
-	public static final String[] VM_STATIC_PROPERTIES = new String[] {
-			PROP_VM_PARENT, PROP_VM_CPU_CORES, PROP_VM_CPU_FREQUENCY, PROP_VM_MEMEORY_TOTAL,
-			PROP_VM_STORAGE_COMMITTED, PROP_VM_STORAGE_UNCOMMITTED,
-			PROP_VM_NETWORK, PROP_STATE, PROP_VM_DISK};
-    
+    public static final String[] VM_STATIC_PROPERTIES = new String[] {
+            PROP_VM_PARENT, PROP_VM_CPU_CORES, PROP_VM_CPU_FREQUENCY,
+            PROP_VM_CPU_USAGE, PROP_VM_MEMEORY_TOTAL, PROP_VM_MEMORY_USAGE,
+            PROP_VM_NETWORK, PROP_STATE, PROP_VM_DISK };
+
     public static final String[] DS_STATIC_PROPERTIES = new String[] {
             PROP_DS_TYPE, PROP_DS_CAPACITY, PROP_DS_FREE_SPACE };
 
-	public static final String[] DYNAMIC_PROPERTIES = new String[] {
-			PROP_CPU_USAGE, PROP_MEM_USAGE, PROP_NET_RX_RATE, PROP_NET_TX_RATE,
-			PROP_NET_USAGE };
+    public static final String[] DYNAMIC_PROPERTIES = new String[] {
+            PROP_NET_RX_RATE, PROP_NET_TX_RATE, PROP_NET_USAGE };
 
-	// non-instantiable
-	private VimServiceConstants() {
-	}
-
+    // non-instantiable
+    private VimServiceConstants() {
+    }
 }
--- src/org/ow2/proactive/iaas/vcloud/monitoring/VimServicePropertyUtil.java
+++ src/org/ow2/proactive/iaas/vcloud/monitoring/VimServicePropertyUtil.java
@@ -1,11 +1,16 @@
 package org.ow2.proactive.iaas.vcloud.monitoring;
 
+import static org.ow2.proactive.iaas.vcloud.monitoring.VimServiceConstants.PROP_HOST_CPU_CORES;
+import static org.ow2.proactive.iaas.vcloud.monitoring.VimServiceConstants.PROP_HOST_CPU_FREQUENCY;
+import static org.ow2.proactive.iaas.vcloud.monitoring.VimServiceConstants.PROP_HOST_CPU_USAGE;
 import static org.ow2.proactive.iaas.vcloud.monitoring.VimServiceConstants.PROP_HOST_MEMORY_TOTAL;
-import static org.ow2.proactive.iaas.vcloud.monitoring.VimServiceConstants.PROP_HOST_MEM_USED;
-import static org.ow2.proactive.iaas.vcloud.monitoring.VimServiceConstants.PROP_MEM_USAGE;
+import static org.ow2.proactive.iaas.vcloud.monitoring.VimServiceConstants.PROP_HOST_MEMORY_USAGE;
+import static org.ow2.proactive.iaas.vcloud.monitoring.VimServiceConstants.PROP_VM_CPU_CORES;
+import static org.ow2.proactive.iaas.vcloud.monitoring.VimServiceConstants.PROP_VM_CPU_FREQUENCY;
+import static org.ow2.proactive.iaas.vcloud.monitoring.VimServiceConstants.PROP_VM_CPU_USAGE;
+import static org.ow2.proactive.iaas.vcloud.monitoring.VimServiceConstants.PROP_VM_MEMEORY_TOTAL;
+import static org.ow2.proactive.iaas.vcloud.monitoring.VimServiceConstants.PROP_VM_MEMORY_USAGE;
 
-import java.text.DecimalFormat;
-import java.text.DecimalFormatSymbols;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -16,27 +21,10 @@ public class VimServicePropertyUtil {
         public static void standardize(Map<String, String> propertyMap) {
             replaceKeyIfPresent(VimServiceConstants.PROP_VM_PARENT, "host",
                     propertyMap);
-            replaceKeyIfPresent(VimServiceConstants.PROP_VM_CPU_CORES,
-                    "cpu.cores", propertyMap);
-
-            // adjust value, MHz -> GHz
-            replaceFrequencyPropertyIfPresent(
-                    VimServiceConstants.PROP_VM_CPU_FREQUENCY, "cpu.frequency",
-                    propertyMap);
-
-            replaceVmMemoryTotalPropertyIfPresent(
-                    VimServiceConstants.PROP_VM_MEMEORY_TOTAL, "memory.total",
-                    propertyMap);
-
-            /*
-            replaceKeyIfPresent(VimServiceConstants.PROP_VM_STORAGE_COMMITTED,
-                    "storage.used", propertyMap);
-            replaceStorageUnCommitted(
-                    VimServiceConstants.PROP_VM_STORAGE_UNCOMMITTED,
-                    "storage.total", propertyMap);
-            */
+            setCupUsageProperties(PROP_VM_CPU_CORES, PROP_VM_CPU_FREQUENCY,
+                    PROP_VM_CPU_USAGE, propertyMap);
+            setVmMemoryUsageProperties(propertyMap);
             setVmStorageUsageProperties(propertyMap);
-            
             replaceKeyIfPresent(VimServiceConstants.PROP_VM_NETWORK,
                     "network.count", propertyMap);
             standardizeCommonProperties(propertyMap);
@@ -45,32 +33,20 @@ public class VimServicePropertyUtil {
 
     public static class HOST {
         public static void standardize(Map<String, String> propertyMap) {
-            replaceKeyIfPresent(VimServiceConstants.PROP_HOST_CPU_CORES,
-                    "cpu.cores", propertyMap);
-            // adjust value, MHz -> GHz
-            replaceFrequencyPropertyIfPresent(
-                    VimServiceConstants.PROP_HOST_CPU_FREQUENCY,
-                    "cpu.frequency", propertyMap);
-//            replaceKeyIfPresent(VimServiceConstants.PROP_HOST_MEMORY_TOTAL,
-//                    "memory.total", propertyMap);
+            setCupUsageProperties(PROP_HOST_CPU_CORES, PROP_HOST_CPU_FREQUENCY,
+                    PROP_HOST_CPU_USAGE, propertyMap);
             setHostMemoryUsageProperties(propertyMap);
+            setHostStorageUsageProperties(propertyMap);
             replaceKeyIfPresent(VimServiceConstants.PROP_HOST_NETWORK_COUNT,
                     "network.count", propertyMap);
             replaceKeyIfPresent(VimServiceConstants.PROP_HOST_SITE, "site",
                     propertyMap);
-            setHostStorageUsageProperties(propertyMap);
             standardizeCommonProperties(propertyMap);
         }
     }
 
     private static void standardizeCommonProperties(
             Map<String, String> propertyMap) {
-        // adjust value
-        replaceUsagePropertyIfPresent(VimServiceConstants.PROP_CPU_USAGE,
-                "cpu.usage", propertyMap);
-        replaceUsagePropertyIfPresent(VimServiceConstants.PROP_MEM_USAGE,
-                "memory.usage", propertyMap);
-        setFreeMemoryProperty(propertyMap);
         replaceKeyIfPresent(VimServiceConstants.PROP_NET_RX_RATE, "network.rx",
                 propertyMap);
         replaceKeyIfPresent(VimServiceConstants.PROP_NET_TX_RATE, "network.tx",
@@ -79,53 +55,116 @@ public class VimServicePropertyUtil {
                 "network.speed", propertyMap);
         replaceStatus(VimServiceConstants.PROP_STATE, "status", propertyMap);
     }
-
-    private static void replaceFrequencyPropertyIfPresent(String oldKey,
-            String newKey, Map<String, String> propertyMap) {
-        String frequency = propertyMap.remove(oldKey);
-        if (frequency != null) {
-            frequency = Float
-                    .toString((float) Long.parseLong(frequency) / 1000);
-            propertyMap.put(newKey, frequency);
-        }
-    }
-
-    private static void replaceVmMemoryTotalPropertyIfPresent(String oldKey,
-            String newKey, Map<String, String> propertyMap) {
-        String total = propertyMap.remove(oldKey);
-        if (total != null) {
-            // convert from Mb to bytes.
-            propertyMap
-                    .put(newKey, Long.toString(Long.parseLong(total) * 1024 * 1024));
-        }
-    }
-
-    private static void replaceUsagePropertyIfPresent(String oldKey,
-            String newKey, Map<String, String> propertyMap) {
-        String usage = propertyMap.remove(oldKey);
-        if (usage != null) {
-            DecimalFormat format = new DecimalFormat();
-            format.setMaximumFractionDigits(4);
-            format.setGroupingUsed(false);
-            DecimalFormatSymbols custom = new DecimalFormatSymbols();
-            custom.setDecimalSeparator('.');
-            format.setDecimalFormatSymbols(custom);
-            usage = format.format(Float.parseFloat(usage) / 10000);
-            propertyMap.put(newKey, usage);
-        }
-    }
-
-    private static void replaceStorageUnCommitted(String oldKey, String newKey,
+    
+    private static void setCupUsageProperties(final String numOfCoresKey,
+            final String cpuFrequencyKey, final String cpuUsageKey,
             Map<String, String> propertyMap) {
-        String storageUncommitted = propertyMap.remove(oldKey);
-        if (storageUncommitted != null) {
-            String storageCommitted = propertyMap.get("storage.used");
-            if (storageCommitted != null) {
-                String total = Long.toString(Long.parseLong(storageUncommitted)
-                        + Long.parseLong(storageCommitted));
-                propertyMap.put(newKey, total);
+        int numOfCores = Integer.valueOf(propertyMap.remove(numOfCoresKey));
+        propertyMap.put("cpu.cores", String.valueOf(numOfCores));
+        
+        float fghz = 0, usage = 0;
+        if (propertyMap.containsKey(cpuFrequencyKey)) {
+            long fMHz = Long.valueOf(propertyMap.remove(cpuFrequencyKey));
+            fghz = ((float) fMHz) / 1000;
+            if (propertyMap.containsKey(cpuUsageKey)) {
+                long uMHz = Long.valueOf(propertyMap.remove(cpuUsageKey));
+                usage = (((float) uMHz) / (numOfCores * fMHz));
             }
         }
+        propertyMap.put("cpu.frequency", String.valueOf(fghz));
+        propertyMap.put("cpu.usage", String.valueOf(usage));
+    }
+
+    private static void setHostMemoryUsageProperties(
+            Map<String, String> propertyMap) {
+        long capacity = 0, used = 0, free =0;
+        if (propertyMap.containsKey(PROP_HOST_MEMORY_TOTAL)) {
+            capacity = Long.valueOf(propertyMap.remove(PROP_HOST_MEMORY_TOTAL));
+        }
+        if (propertyMap.containsKey(PROP_HOST_MEMORY_USAGE)) {
+            used = mbytes2bytes(propertyMap.remove(PROP_HOST_MEMORY_USAGE));
+        }
+        if (capacity > used) {
+            free = capacity - used;
+        }
+        propertyMap.put("memory.total", String.valueOf(capacity));
+        propertyMap.put("memory.used", String.valueOf(used));
+        propertyMap.put("memory.free", String.valueOf(free));
+    }
+    
+    private static void setHostStorageUsageProperties(
+            Map<String, String> propertyMap) {
+        long capacity = 0, free = 0, used = 0;
+        List<String> toRemove = new ArrayList<String>();
+        for (String key : propertyMap.keySet()) {
+            if (key.startsWith("host.datastore.")) {
+                toRemove.add(key);
+                if (key.endsWith(".total")) {
+                    capacity = Long.parseLong(propertyMap.get(key));
+                } else if (key.endsWith(".free")) {
+                    free = Long.parseLong(propertyMap.get(key));
+                }
+                if (capacity != 0 && free != 0) {
+                    break;
+                }
+            }
+        }
+        if (capacity > free) {
+        used = capacity - free;
+        }
+        propertyMap.put("storage.total", String.valueOf(capacity));
+        propertyMap.put("storage.free", String.valueOf(free));
+        propertyMap.put("storage.used", String.valueOf(used));
+        
+        removeAll(toRemove, propertyMap);
+    }
+    
+    private static void setVmMemoryUsageProperties(
+            Map<String, String> propertyMap) {
+        long totalInBytes = 0, usageInBytes = 0, freeInBytes = 0;
+        if (propertyMap.containsKey(PROP_VM_MEMEORY_TOTAL)) {
+            totalInBytes = mbytes2bytes(propertyMap
+                    .remove(PROP_VM_MEMEORY_TOTAL));
+        }
+        if (propertyMap.containsKey(PROP_VM_MEMORY_USAGE)) {
+            usageInBytes = mbytes2bytes(propertyMap
+                    .remove(PROP_VM_MEMORY_USAGE));
+        }
+        if (totalInBytes > usageInBytes) {
+            freeInBytes = totalInBytes - usageInBytes;
+        }
+        propertyMap.put("memory.total", String.valueOf(totalInBytes));
+        propertyMap.put("memory.used", String.valueOf(usageInBytes));
+        propertyMap.put("memory.free", String.valueOf(freeInBytes));
+    }
+    
+    private static void setVmStorageUsageProperties(
+            Map<String, String> propertyMap) {
+        long total = 0;
+        long free = 0;
+        long used = 0;
+        List<String> toRemove = new ArrayList<String>();
+        for (String key : propertyMap.keySet()) {
+            if (key.startsWith("vm.disk.")) {
+                toRemove.add(key);
+                long value = Long.valueOf(propertyMap.get(key));
+                if (key.endsWith(".total")) {
+                    total += value;
+                } else if (key.endsWith(".free")) {
+                    free += value;
+                } else if (key.endsWith(".used")) {
+                    used += value;
+                } else {
+                    // FIXME: consider throwing an exception
+                }
+            }
+        }
+        
+        propertyMap.put("storage.total", String.valueOf(total));
+        propertyMap.put("storage.free", String.valueOf(free));
+        propertyMap.put("storage.used", String.valueOf(used));
+        
+        removeAll(toRemove, propertyMap);
     }
 
     private static void replaceStatus(String oldKey, String newKey,
@@ -144,96 +183,16 @@ public class VimServicePropertyUtil {
         }
     }
     
-    private static void setHostMemoryUsageProperties(
-            Map<String, String> propertyMap) {
-        long capacity = -1, used = -1;
-        if (propertyMap.containsKey(PROP_HOST_MEMORY_TOTAL)) {
-            capacity = Long.valueOf(propertyMap.get(PROP_HOST_MEMORY_TOTAL));
-            propertyMap.put("memory.total", String.valueOf(capacity));
-            propertyMap.remove(PROP_HOST_MEMORY_TOTAL);
-        }
-        if (propertyMap.containsKey(PROP_HOST_MEM_USED)) {
-            used = Long.valueOf(propertyMap.get(PROP_HOST_MEM_USED)) * 1024 * 1024;
-            propertyMap.put("memory.used", String.valueOf(used));
-            if (capacity != -1) {
-                String free = String.valueOf(capacity - used);
-                propertyMap.put("memory.free", free);
-                // FIXME:
-                propertyMap.put("memory.actualfree", free);
-                propertyMap.remove(PROP_HOST_MEM_USED);
-            }
-        }
-        
-        propertyMap.remove(PROP_MEM_USAGE);
+    private static long mbytes2bytes(String value) {
+        return Long.parseLong(value) * 1024 * 1024;        
     }
-
-    private static void setFreeMemoryProperty(Map<String, String> propertyMap) {
-        String total = propertyMap.get("memory.total");
-        String usage = propertyMap.get("memory.usage");
-        if (total != null && usage != null) {
-            float free = Long.parseLong(total)
-                    * (1 - Float.parseFloat(usage) / 100);
-            propertyMap.put("memory.free", Long.toString((long) free));
-            propertyMap.put("memory.actualfree", Long.toString((long) free));
+    
+    private static void removeAll(List<String> keys, Map<String,String> properties) {
+        for (String key : keys) {
+            properties.remove(key);
         }
     }
-
-    private static void setVmStorageUsageProperties(
-            Map<String, String> propertyMap) {
-        long total = 0;
-        long free = 0;
-        long used = 0;
-        List<String> vmDiskInfoKeys = new ArrayList<String>();
-        for (String key : propertyMap.keySet()) {
-            if (key.startsWith("vm.disk.")) {
-                vmDiskInfoKeys.add(key);
-                long value = Long.valueOf(propertyMap.get(key));
-                if (key.endsWith(".total")) {
-                    total += value;
-                } else if (key.endsWith(".free")) {
-                    free += value;
-                } else if (key.endsWith(".used")) {
-                    used += value;
-                } else {
-                    // FIXME: consider throwing an exception
-                }
-            }
-        }
-        for (String key : vmDiskInfoKeys) {
-            propertyMap.remove(key);
-        }
-        propertyMap.put("storage.total", String.valueOf(total));
-        propertyMap.put("storage.free", String.valueOf(free));
-        propertyMap.put("storage.used", String.valueOf(used));
-    }
-     
-    private static void setHostStorageUsageProperties(Map<String,String> propertyMap) {
-        List<String> hostDataStoreKeys = new ArrayList<String>();
-        long capacity = -1, free = -1;
-        for (String key: propertyMap.keySet()) {
-            if (key.startsWith("host.datastore.")) {
-                hostDataStoreKeys.add(key);
-                if (key.endsWith(".total")) {
-                    capacity = Long.parseLong(propertyMap.get(key));
-                } else if (key.endsWith(".free")) {
-                    free = Long.parseLong(propertyMap.get(key));
-                }
-                if (capacity != -1 && free != -1 ) {
-                    break;
-                }
-            }
-        }
-        free = (free == -1 ) ? 0 : free;
-        long used = capacity - free;
-        propertyMap.put("storage.total", String.valueOf(capacity));
-        propertyMap
-                .put("storage.free", String.valueOf(free));
-        propertyMap.put("storage.used", String.valueOf(used));
-        for (String key : hostDataStoreKeys) {
-            propertyMap.remove(key);
-        }
-    }
-
+    
     // non-instantiable
     private VimServicePropertyUtil() {
     }

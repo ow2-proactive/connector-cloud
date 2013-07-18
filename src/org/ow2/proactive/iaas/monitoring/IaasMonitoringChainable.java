40247c48322084e80204c7b8f4aee94146f3518e
Mauricio Jost <mauricio.jost@activeeon.com>
2013-07-18 09:24:57 +0200

    Monitoring. Added some documentation and qemu support for VMProcesses.
    Change-Id: I8c4a4e541ae38da2b4b67a53302f5e30bf25df9e
    Signed-off-by: Mauricio Jost <mauricio.jost@activeeon.com>

--- src/org/ow2/proactive/iaas/monitoring/IaasMonitoringChainable.java
+++ src/org/ow2/proactive/iaas/monitoring/IaasMonitoringChainable.java
@@ -2,12 +2,24 @@ package org.ow2.proactive.iaas.monitoring;
 
 import org.ow2.proactive.iaas.IaasMonitoringApi;
 
+
+/**
+ * Asking for a cpu.usage might involve going through several levels of 
+ * monitoring, for instance, asking to the API of the infrastructure (VMWare), or 
+ * using the Sigar MBean exposed by the RMNode running on the target node.
+ * 
+ *                                                         +-->   [  api  ] 
+ * get("cpu.usage") <-> [ cacher ] <-> [ monit merger ] <--|  
+ *                                                         +-->   [ sigar ]
+ * 
+ * Every element on this chain should implement this interface.
+ */
 public interface IaasMonitoringChainable extends IaasMonitoringApi, IaasNodesListener {
     /**
      * Configure the block.
      */
     public void configure(String nsname, String options);
-    
+
     /**
      * ShutDown the block.
      */

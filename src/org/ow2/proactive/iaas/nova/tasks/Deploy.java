a65c1576cf088484f5ea59a4eab4531b0a869fbc
Mauricio Jost <mauricio.jost@activeeon.com>
2013-09-24 11:57:29 +0200

    Added tasks Deploy and Undeploy for Nova infrastructure.
    Change-Id: Ifeaea45e6ea72d353b616a3b5774ca20dd4caeb1
    Signed-off-by: Mauricio Jost <mauricio.jost@activeeon.com>

--- src/org/ow2/proactive/iaas/nova/tasks/Deploy.java
+++ src/org/ow2/proactive/iaas/nova/tasks/Deploy.java
@@ -1,71 +1,40 @@
 package org.ow2.proactive.iaas.nova.tasks;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpPut;
-import org.apache.http.impl.client.DefaultHttpClient;
+import net.minidev.json.JSONObject;
+import org.ow2.proactive.iaas.IaasApi;
 import org.ow2.proactive.iaas.IaasExecutable;
 import org.ow2.proactive.iaas.IaasInstance;
 import org.ow2.proactive.iaas.nova.NovaAPI;
-import org.ow2.proactive.iaas.vcloud.VCloudAPI;
 import org.ow2.proactive.scheduler.common.task.TaskResult;
-import org.ow2.proactive.scripting.PropertyUtils;
-
-import java.io.IOException;
 import java.io.Serializable;
-import java.util.HashMap;
-import java.util.Map;
 
 
 public class Deploy extends IaasExecutable {
 
     @Override
     public Serializable execute(TaskResult... results) throws Throwable {
-        NovaAPI api = null;
-        String instanceId = null;
-
+        IaasApi api = null;
         try {
-            api = (NovaAPI) createApi(args);
-            instanceId = startInstance(api);
+            api = createApi(args);
+            String instanceId = startInstance(api);
+            return getJsonString(instanceId);
         } catch (Throwable e) {
-            return e;
+            throw e;
         } finally {
-            if (api != null) {
+            if (api != null)
                 api.disconnect();
-            }
         }
-
-        HttpResponse resp = updateOcciServer(args, instanceId);
-        System.out.println("Update result: " + resp.getStatusLine().getStatusCode() + ":" + resp.getStatusLine().getReasonPhrase());
-        return instanceId;
     }
 
-    private String startInstance(NovaAPI api) throws Exception {
+    private String getJsonString(String instanceId) {
+        JSONObject o = new JSONObject();
+        o.put("occi.compute.vendor.uuid", instanceId);
+        return o.toString();
+    }
+
+    private String startInstance(IaasApi api) throws Exception {
         IaasInstance instance = api.startInstance(args);
         return instance.getInstanceId();
     }
-
-    public HttpResponse updateOcciServer(Map<String, String> args, String instanceId) throws IOException {
-
-        String endpoint = args.get("occi_endpoint").toString();
-        String category = args.get("category").toString();
-        String occi_core_id = args.get("occi.core.id").toString();
-        String occi_url = endpoint + category + "/" + occi_core_id;
-
-        System.out.println("Updating " + occi_url);
-
-        Map<String, String> attributeMap = new HashMap<String, String>();
-        attributeMap.put("uuid", instanceId);
-        HttpPut put = new HttpPut(occi_url);
-        String attributes = "";
-        String comma = "";
-        for (String key : attributeMap.keySet()) {
-            attributes += comma + key + "=" + attributeMap.get(key);
-            comma = ",";
-        }
-
-        put.addHeader("X-OCCI-Attribute", attributes);
-        return new DefaultHttpClient().execute(put);
-    }
-
 
 }
--- src/org/ow2/proactive/iaas/nova/tasks/Undeploy.java
+++ src/org/ow2/proactive/iaas/nova/tasks/Undeploy.java
@@ -1,34 +1,38 @@
 package org.ow2.proactive.iaas.nova.tasks;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.util.EntityUtils;
+import net.minidev.json.JSONObject;
+import org.ow2.proactive.iaas.IaasApi;
 import org.ow2.proactive.iaas.IaasExecutable;
 import org.ow2.proactive.iaas.IaasInstance;
-import org.ow2.proactive.iaas.vcloud.VCloudAPI;
-import org.ow2.proactive.iaas.vcloud.VCloudAPI.VCloudAPIConstants;
 import org.ow2.proactive.scheduler.common.task.TaskResult;
-import org.ow2.proactive.scripting.PropertyUtils;
-
 import java.io.Serializable;
-import java.util.HashMap;
 import java.util.Map;
-
 
 public class Undeploy extends IaasExecutable {
 
-    public static final String INSTANCE_ID_KEY = "uuid";
-
+    public static final String INSTANCE_ID_KEY = "occi.compute.vendor.uuid";
 
     @Override
     public Serializable execute(TaskResult... results) throws Throwable {
-        String instanceId;
+        IaasApi api = null;
 
-        instanceId = getInstanceId(args, results);
+        try {
+            api = createApi(args);
+            String instanceId = getInstanceId(args, results);
+            api.stopInstance(new IaasInstance(instanceId));
+            return getJsonString("deleted");
+        } catch (Throwable e) {
+            throw e;
+        } finally {
+            if (api != null)
+                api.disconnect();
+        }
+    }
 
-        createApi(args).stopInstance(new IaasInstance(instanceId));
-        return "DONE";
+    private String getJsonString(String status) {
+        JSONObject o = new JSONObject();
+        o.put("occi.compute.state", status);
+        return o.toString();
     }
 
     private String getInstanceId(Map<String, String> args, TaskResult[] results) throws Throwable {
@@ -38,36 +42,10 @@ public class Undeploy extends IaasExecutable {
         if (instanceId != null && !instanceId.isEmpty())
             return instanceId;
 
-        instanceId = getInstanceIdFromOcciServer(args);
-        if (instanceId != null && !instanceId.isEmpty())
-            return instanceId;
-
         if (results.length > 0)
             return (String) results[0].value();
 
         throw new IllegalArgumentException("VM uuid not provided.");
-    }
-
-    public String getInstanceIdFromOcciServer(Map<String, String> args) throws Throwable {
-
-        String endpoint = args.get("occi_endpoint").toString();
-        String category = args.get("category").toString();
-        String occi_core_id = args.get("occi.core.id").toString();
-        String attributeKey = args.get("attribute").toString();
-        String occi_url = endpoint + category + "/" + occi_core_id;
-
-        String fullUrl = occi_url + "?attribute=" + attributeKey;
-        HttpGet get = new HttpGet(fullUrl);
-        HttpResponse response = new DefaultHttpClient().execute(get);
-        String attributeValue = EntityUtils.toString(response.getEntity());
-
-        return attributeValue;
-    }
-
-
-    @Override
-    public void init(Map<String, Serializable> args) throws Exception {
-        super.init(args);
     }
 
 }
